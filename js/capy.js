0960bd5e7da312c3b0fcbba76c226c04fa9a73db
anna <aniajastrzebska2@gmail.com>
2022-07-16 08:14:29 +0200

    szybko

--- js/capy.js
+++ js/capy.js
@@ -4,6 +4,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
   document.querySelectorAll('img').forEach((el)=>{
     setTimeout(()=>{
       el.classList.add("animation-spin")
+      el.style.animationTimingFunction = `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`
+      el.style.animationDuration = Math.random() > 0.5 ? `${Math.random() * 50}ms` : `${Math.random() * 5000 + 2500}ms`
     }, Math.random() * 1200 + 300)
   })
 });
